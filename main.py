1fa1888f31d4927267634c41eda485a4838d7c80
beyond-time <88.mushroom.88@gmail.com>
2025-08-13 21:31:00 -0700

    Update game files

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
 import pygame
 from constants import *
 
-def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
 
+def main():
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
-        pygame.Surface.fill(screen, (0, 0, 0))
+
+        screen.fill("black")
         pygame.display.flip()
-        #time.sleep(1)
-    
+
+
 if __name__ == "__main__":
     main()
+    
